427bcc955da51b2fd52a933522970a792670772d
MoistDreams <22002023+Misunderstood-Wookiee@users.noreply.github.com>
2024-06-12 02:55:47 +1000

    The Stuff...

---
+++ Discord/Discord WebApp/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Discord_WebApp
+{
+    /// <summary>
+    /// Core Display Page for Stremio Content.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public static string DeviceForm { get; } // Get Device Info
+        public MainPage()
+        {
+            this.InitializeComponent();
+            Environment.SetEnvironmentVariable("WEBVIEW2_DEFAULT_BACKGROUND_COLOR", "151330"); // Control WebView2 default background colour
+        }
+
+    }
+}
